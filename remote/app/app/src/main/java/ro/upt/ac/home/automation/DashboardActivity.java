f0b1f1537ad736999a4e774bf2064604d57c3335
Luigi Bolovan <luigibolovan@gmail.com>
2021-05-13 00:18:07 +0300

    added dashboard, login and register layouts

---
+++ remote/app/app/src/main/java/ro/upt/ac/home/automation/DashboardActivity.java
@@ -0,0 +1,14 @@
+package ro.upt.ac.home.automation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
