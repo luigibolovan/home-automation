d24f20bfbec439148d7ee5d935a771ba5b0de3d2
Luigi Bolovan <luigibolovan@gmail.com>
2021-05-13 23:34:11 +0300

    door lock and lights layout

---
+++ remote/app/app/src/main/java/ro/upt/ac/home/automation/DoorLockActivity.java
@@ -0,0 +1,14 @@
+package ro.upt.ac.home.automation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DoorLockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_door_lock);
+    }
+}
